2bd42852f5fb04eaf92796dedb9de8aa5bbb95ca
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-13 13:34:30 +0530

    modified sh file  in repo

--- terraform-installation.sh
+++ terraform-installation.sh
@@ -14,6 +14,7 @@ validate_function(){
         sudo yum install -y yum-utils
         sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
         sudo yum -y install terraform
+    fi
 }
 
 for i in $packages
